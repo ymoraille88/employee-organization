05f77bfec9d24c15053a52ad11eb51703f720afd
Yves-Henry Moraille <yves-henrymoraille@Yves-Henrys-Air.lan>
2023-02-05 15:16:50 -0500

    fixed all the error codes

--- db/schema.sql
+++ db/schema.sql
@@ -1,26 +1,27 @@
+DROP DATABASE IF EXISTS employees_db;
 
 CREATE DATABASE if not exists employees_db;
-
-DROP DATABASE IF EXISTS employees_db;
-CREATE TABLE department_db;
-id INT NOT NULL PRIMARY KEY,
-  name VARCHAR(30) NOT NULL
-);
-CREATE TABLE role_db(
-id INT NOT NULL PRIMARY KEY,
-  title VARCHAR(30) NOT NULL
-  salary DECIMAL NOT NULL
-  department_id INT NOT NULL
-);
-CREATE TABLE employee_db(
-id INT NOT NULL PRIMARY KEY,
-  first_name VARCHAR(30) NOT NULL
-  last_name VARCHAR(30) NOT NULL
-  role_id INT
-  manager_id INT NULL
-);
-
 USE employees_db;
 
+CREATE TABLE department(
+        id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        name VARCHAR(30) NOT NULL
+    );
 
-SELECT TABLE();
+CREATE TABLE role(
+        id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        title VARCHAR(30) NOT NULL, 
+        salary DECIMAL NOT NULL,
+        department_id INT NOT NULL,
+        FOREIGN KEY (department_id) REFERENCES department(id)
+    );
+
+CREATE TABLE employee(
+        id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        first_name VARCHAR(30) NOT NULL,
+        last_name VARCHAR(30) NOT NULL,
+        role_id INT,
+        manager_id INT NULL,
+        FOREIGN KEY (role_id) REFERENCES role(id),
+        FOREIGN KEY (manager_id) REFERENCES employee(id)
+    );
